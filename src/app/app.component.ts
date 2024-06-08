5dec89f6d73186a78ec84b4adfc21908e7a41b38
SYL Multimedia <admin@sylmultimedia.com>
2024-06-08 11:56:42 +0100

    Updated Android splash screen images2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@awesome-cordova-plugins/splash-screen/ngx';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
   imports: [IonApp, IonRouterOutlet],
+  providers: [SplashScreen]
 })
 export class AppComponent {
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform, private splashScreen: SplashScreen) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      if (navigator && navigator.splashscreen) {
-        navigator.splashscreen.hide();
-      }
+      this.splashScreen.hide();
     });
   }
 }
