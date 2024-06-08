ce72f8af796f248a469df116513d8aee706e64b3
SYL Multimedia <admin@sylmultimedia.com>
2024-06-08 11:39:31 +0100

    Updated Android splash screen images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,15 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      if (navigator && navigator.splashscreen) {
+        navigator.splashscreen.hide();
+      }
+    });
+  }
 }
