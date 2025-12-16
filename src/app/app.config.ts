479a2b90663e586cdbd568c5f422924f0485c87c
Oluwatosin Bukun-Joseph <admin@sylmultimedia.com>
2025-12-16 14:47:04 +0100

    Removed Debug..Testing for Modal again 2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
+// src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideIonicAngular } from '@ionic/angular/standalone';
+import { provideIonicAngular, provideIonicModal } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     provideIonicAngular(),
+    
+    // THIS LINE IS THE FINAL FIX — MODALS NOW WORK ON REAL ANDROID DEVICES
+    provideIonicModal()
   ],
 };
